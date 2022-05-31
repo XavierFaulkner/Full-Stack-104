437ed46f266d6d839bfbc1a24284a4c8f484d4c9
Xavier Faulkner <xjfaulkner@gmail.com>
2022-05-31 08:50:39 -0400

    Added unversioned files

---
+++ Day_65/course-api/src/test/java/com/ctac/courseapi/CourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctac.courseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
